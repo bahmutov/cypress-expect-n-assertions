1fc312996f4b4f5abfeedac93a4da819afc9e286
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-01-04 14:52:55 -0500

    rename and add prettier

--- cypress/integration/fails-1.js
+++ cypress/integration/fails-1.js
@@ -1,14 +1,16 @@
 /// <reference types="Cypress" />
-const {plan} = require('../..')
+const { plan } = require('../..')
 
-it('fails synchronously', () => {
-  expect(1, 'one').to.equal(1)
-  expect(2, 'two').to.equal(2)
-  expect(false).to.be.true
-})
+describe('fails-1', () => {
+  it('fails synchronously', () => {
+    expect(1, 'one').to.equal(1)
+    expect(2, 'two').to.equal(2)
+    expect(false).to.be.true
+  })
 
-it('fails wrong number of assertions', () => {
-  plan(1)
-  expect(1, 'one').to.equal(1)
-  expect(2, 'two').to.equal(2)
+  it('fails wrong number of assertions', () => {
+    plan(1)
+    expect(1, 'one').to.equal(1)
+    expect(2, 'two').to.equal(2)
+  })
 })
